9e3ce17d991ac9f15b927efa07558956614867e2
Roman Kordyuk <kordukroman@gmail.com>
2019-02-01 12:18:44 +0200

    fixes routing and added some cool css in star wars theme"

--- src/movieDetails.jsx
+++ src/movieDetails.jsx
@@ -1,41 +1,59 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import SideBar from './sideBar.jsx';
+import styles from './Styles/sideBar.module.css';
 
 class movieDetails extends Component {
-    state = {
-        data:null,
-        loading:true
-    }
-    componentDidMount(){
-          let url = "https://swapi.co/api/films";
-          axios.get(url).then((response)=>{
-            this.setState((prevState)=>{
-                return({
-                    data:response.data.results,
-                    loading:false
-                });
-            });
-          }, (err) => {
-              console.log("rejected");
-          });
-    }
-    render(){
-        const {data,loading} = this.state;
-        if(loading)return <h1>Loading...</h1>;
-        let id = Number(this.props.match.params.id);
-        let optimization = (data.filter((item)=>{return item.episode_id==id}))[0];
-        return (<div>
-            <SideBar loading={loading} data={data}></SideBar>
-            <ul>
-                <li>title: {optimization.title}</li>
-                <li>Opening crawl: {optimization.opening_crawl}</li>
-                <li>Director: {optimization.director}</li>
-                <li>Producers: {optimization.producer}</li>
-                <li>Release date: {optimization.release_date}</li>
-            </ul>
-        </div>)
-    }
+	state = {
+		data: null,
+		loading: true
+	};
+	componentDidMount() {
+		let url = 'https://swapi.co/api/films';
+		axios.get(url).then(
+			response => {
+				this.setState(prevState => {
+					return {
+						data: response.data.results,
+						loading: false
+					};
+				});
+			},
+			err => {
+				console.log('rejected');
+			}
+		);
+	}
+	render() {
+		const { data, loading } = this.state;
+		if (loading) return <h1>Loading...</h1>;
+		let id = Number(this.props.match.params.id);
+		let optimization = data.filter(item => {
+			return item.episode_id == id;
+		})[0];
+		return (
+			<div>
+				<SideBar loading={loading} data={data} />
+				<h1>
+					STAR WARS
+				</h1>
+				<div
+					className={styles.titles}
+					// marginLeft : '200px',
+					// width: '500px'
+				>
+					<div className={styles.titlecontent}>
+						<p>Episode: {optimization.episode_id}</p>
+						<p>Title: {optimization.title}</p>
+						<p>Opening crawl: {optimization.opening_crawl}</p>
+						<p>Director: {optimization.director}</p>
+						<p>Producers: {optimization.producer}</p>
+						<p>Release date: {optimization.release_date}</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default movieDetails;
--- src/sideBar.jsx
+++ src/sideBar.jsx
@@ -1,36 +1,44 @@
 import React, { Component } from 'react';
 import Link from 'react-router-dom/Link';
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import SideNav, {
+	Toggle,
+	Nav,
+	NavItem,
+	NavIcon,
+	NavText
+} from '@trendmicro/react-sidenav';
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
-
 class SideBar extends Component {
-    render() {
-        if(this.props.loading)return <h1>Loading...</h1>;/*
+	render() {
+		if (this.props.loading)
+			return (
+				<h1>Loading...</h1>
+			); /*
         return <div><ul>{this.props.data.map((item,index)=>{
             return (<li><Link key={item.episode_id} to={"movies/" + item.episode_id}>
             {item.title}</Link></li>)
         })}</ul></div>*/
-        return (
-            <SideNav
-                onSelect={(selected) => {
-                    
-                }}
-            >
-                <SideNav.Nav>
-                    {this.props.data.map((item, index) => {
-                        return (
-                            <NavItem eventKey={'movies/' + item.episode_id}>
-                                <Link key={item.episode_id} to={'movies/' + item.episode_id}></Link>
-                                <NavText>{item.title}</NavText>
-                            </NavItem>
-                        );
-                    })}
-                </SideNav.Nav>
-            </SideNav>
-
-        )
-    }
+		return (
+			<React.Fragment>
+				<SideNav style={{ backgroundColor: '#1a1400'}}>
+					<SideNav.Nav style={{ width: '180px'}}>
+						{this.props.data.map((item, index) => {
+							return (
+								<NavItem eventKey={'/movies/' + item.episode_id}>
+									<Link style={{ color: '#ff6', paddingLeft: '5px', textDecoration: 'none'  }}
+										key={item.episode_id}
+										to={'/movies/' + item.episode_id}
+									>{item.title}</Link>
+									
+								</NavItem>
+							);
+						})}
+					</SideNav.Nav>
+				</SideNav>
+			</React.Fragment>
+		);
+	}
 }
 
 export default SideBar;
